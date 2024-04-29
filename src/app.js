c9b4a53b2a5df329c5631b3459dabd10ef321534
guntar <guntar@me.com>
2024-04-29 11:04:40 +0700

    Setup template dasar handlebars dan halaman terkait Tambah template laman chat Update style.css

--- src/app.js
+++ src/app.js
@@ -1,69 +1,68 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const express = require('express');
+const path = require('path');
+const { engine } = require('express-handlebars');
 
-const app = express()
+const app = express();
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+const layoutsPath = path.join(__dirname, '../templates/layouts');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-
-app.use(express.static("public"));
+app.engine(
+  'hbs',
+  engine({
+    extname: 'hbs', // sets the extension name to .hbs
+    defaultLayout: 'main', // sets the default layout to main.hbs
+    layoutsDir: layoutsPath, // path to layouts folder
+    partialsDir: partialsPath, // path to partials folder
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather',
-        name: 'Andrew Mead'
-    })
-})
+  res.render('index', {
+    title: 'Home',
+  });
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
-    })
-})
+app.get("/chats", (req, res) => {
+  res.render("chats", {
+    title: "Chats",
+  });
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
-    })
-})
+app.get('/datachat', (req, res) => {
+  res.render('datachat', {
+    title: 'Data Chat',
+  });
+});
 
-app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
-})
+app.get("/pages", (req, res) => {
+  res.render("pages", {
+    title: "Pages",
+  });
+});
 
-app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Help article not found.'
-    })
-})
+app.get("/forms", (req, res) => {
+  res.render("forms", {
+    title: "Forms",
+  });
+});
+
 
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Andrew Mead',
-        errorMessage: 'Page not found.'
-    })
-})
+  res.render('404', {
+    title: '404',
+  });
+});
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log('Server is up on port 3000.');
+});
