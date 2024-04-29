bc3ef08bb790b6860e46c59a7a4c52d6e2acf28d
guntar <guntar@me.com>
2024-04-29 14:39:41 +0700

    Chat can now display messages of each person

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { engine } = require('express-handlebars');
 
 const app = express();
+const fs = require('fs');
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -12,12 +13,22 @@ const layoutsPath = path.join(__dirname, '../templates/layouts');
 
 // Setup handlebars engine and views location
 app.engine(
-  'hbs',
+  "hbs",
   engine({
-    extname: 'hbs', // sets the extension name to .hbs
-    defaultLayout: 'main', // sets the default layout to main.hbs
+    extname: "hbs", // sets the extension name to .hbs
+    defaultLayout: "main", // sets the default layout to main.hbs
     layoutsDir: layoutsPath, // path to layouts folder
     partialsDir: partialsPath, // path to partials folder
+    helpers: {
+      truncate: function (str, numWords) {
+        var words = str.split(" ");
+        if (words.length > numWords) {
+          words = words.slice(0, numWords);
+          return words.join(" ") + "...";
+        }
+        return str;
+      },
+    },
   })
 );
 app.set('view engine', 'hbs');
@@ -32,27 +43,50 @@ app.get('', (req, res) => {
   });
 });
 
-app.get("/chats", (req, res) => {
-  res.render("chats", {
-    title: "Chats",
+app.get('/chats', (req, res) => {
+  const chatsPath = path.join(__dirname, "chatsData.json");
+  fs.readFile(chatsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .send("An error occurred while reading the chats.json file.");
+    }
+    const chats = JSON.parse(data);
+    res.render("chats", {
+      title: "Chats",
+      chats: chats,
+    });
   });
 });
 
+app.get("/api/chats", (req, res) => {
+  const chatsPath = path.join(__dirname, "chatsData.json");
+  fs.readFile(chatsPath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .send("An error occurred while reading the chatsData.json file.");
+    }
+    res.json(JSON.parse(data));
+  });
+});
 app.get('/datachat', (req, res) => {
   res.render('datachat', {
     title: 'Data Chat',
   });
 });
 
-app.get("/pages", (req, res) => {
-  res.render("pages", {
-    title: "Pages",
+app.get('/pages', (req, res) => {
+  res.render('pages', {
+    title: 'Pages',
   });
 });
 
-app.get("/forms", (req, res) => {
-  res.render("forms", {
-    title: "Forms",
+app.get('/forms', (req, res) => {
+  res.render('forms', {
+    title: 'Forms',
   });
 });
 
